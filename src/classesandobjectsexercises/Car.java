dfa6ee02f632017ba6c9ad06e01259483229d51a
Ana Claudia de Proenca <ac-proenca@hotmai.com>
2023-12-01 16:00:08 +0000

    Created Car Class and properties
    make, model, numWheels, colour

---
+++ src/classesandobjectsexercises/Car.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package classesandobjectsexercises;
+
+/**
+ *
+ * @author anaclaudiaproenca
+ */
+public class Car {
+    String make;
+    String model;
+    int numWheels;
+    String colour;
+    
+}
