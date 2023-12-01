b3e5fb5f4c05f81f1ba6570ad24eab1754b31dd5
Ana Claudia de Proenca <ac-proenca@hotmai.com>
2023-12-01 17:17:58 +0000

    made car1 and constructor

--- src/classesandobjectsexercises/Car.java
+++ src/classesandobjectsexercises/Car.java
@@ -13,10 +13,17 @@ public class Car {
     String model;
     int numWheels;
     String colour;
-    
-    public void drive(double direction) {
-        System.out.println(String.format("The %s %s is diving at %f degrees", colour, make, direction));
-        System.out.printf("The %s %s is diving at %f degrees", colour, make, direction);
+
+    public Car(String make, String model, int numWheels, String colour) {
+        this.make = make;
+        this.model = model;
+        this.numWheels = numWheels;
+        this.colour = colour;
     }
-    
+        
+    public void drive(double direction) {
+        System.out.println(String.format("The %s %s is diving at %.0f degrees", colour, make, direction));
+        System.out.printf("The %s %s is diving at %.0f degrees", colour, make, direction);
+    }
+     
 }
